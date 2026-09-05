cac9e9f1e3d75134d14e0bb2bf189ac8c4857832
Haresh Prajapati <pharesh363@email.com>
2026-09-05 15:48:42 +0530

    Add debug output to action_report_data function for row inspection

--- admin/ajax/handler.php
+++ admin/ajax/handler.php
@@ -197,6 +197,8 @@ function action_report_data(mysqli $con): void
             }
         }
     }
+    echo '<pre>'; print_r($rows); echo '</pre>'; exit;
+
     ?>
 <div class="hp-card">
     <div class="hp-card-header">
